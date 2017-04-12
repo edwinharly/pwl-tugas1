59876ecb3c1d20d6950df703d5cef2aebdd90e0d
Edwin Harly <edwinharly@live.com>
2017-04-12 14:52:29 +0700

    using pug templating

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-//const pug = require('pug')
+// const pug = require('pug')
 
-
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    let judul = ''
-    //let compiledFunction = pug.compileFile('home.pug')
+    // let judul = ''
+    // let compiledFunction = pug.compileFile('home.pug')
 
     fs.readdir('cerita/', 'utf8', (err, files) => {
         if (err) {
             throw err
         }
-        for (let file of files) {
-            judul += `<a href="/${ file.split('.')[0] }">`
-            judul += file.split('.')[0]
-            judul += '</a><br/>'
-        }
-        res.send(judul)
+        
+        // for (let file of files) {
+        //     judul += `<a href="/${ file.split('.')[0] }">`
+        //     judul += file.split('.')[0]
+        //     judul += '</a><br/>'
+        // }
+        
+        // res.send(judul)
+        res.render('home', {
+            files: files
+        })
     })
 })
 
 app.get('/:judul', (req, res, next) => {
-    let isi = ''
+    // let isi = ''
     fs.readFile('cerita/' + req.params.judul + '.txt', 'utf8', (err, data) => {
         if (err) {
             next()
         }
-        isi += '<p>' + data + '</p>' + '<a href="/">Back</a>'
-        res.send(isi)
+        // isi += '<p>' + data + '</p>' + '<a href="/">Back</a>'
+        // res.send(isi)
+        res.render('cerita', {
+            data: data
+        })
     })
 })
 
